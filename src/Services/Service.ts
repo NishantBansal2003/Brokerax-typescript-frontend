f0e0252456785933a5562d9230db9eab4b2f8a45
Nishant Bansal <nishant.bansal.mec21@iitbhu.ac.in>
2024-08-04 10:42:07 +0530

    Brokerax Deployed

--- src/Services/Service.ts
+++ src/Services/Service.ts
@@ -1,3 +1,3 @@
-const BASE_URL: string = "http://localhost:4001";
-// const BASE_URL = "https://brokeraxbackend.onrender.com";
+// const BASE_URL: string = "http://localhost:4001";
+const BASE_URL = "https://brokerax-golang-backend.onrender.com";
 export default BASE_URL;
